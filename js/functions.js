96a798819db0fc30373942fa2f3b8e5b911db2ae
sabinalebediene <70894973+sabinalebediene@users.noreply.github.com>
2020-09-30 14:22:27 +0300

    Initial FB

---
+++ js/functions.js
@@ -0,0 +1,15 @@
+// Pirma importuoja
+
+import posts from './data.js';
+// import suma from './suma.js';
+// import atimtis from './atimtis.js';
+// import sayHi from './sayHi.js';
+
+// // po to panaudoja 
+
+// console.log(posts);
+
+// console.log(suma(4, 8));
+// console.log(atimtis(14, 8));
+
+// console.log(sayHi('Rex'));
